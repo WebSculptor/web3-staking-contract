4fb4816a0f2893e465a53e77f1cce061ab91c9c0
Abdullahi Salihu <abdullahisalihuinusa@gmail.com>
2024-02-17 19:27:53 +0100

    Custom errors

--- contracts/StakeThisGuy.sol
+++ contracts/StakeThisGuy.sol
@@ -3,10 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-error AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
-error BALANCE_IS_LESS_THAN_AMOUNT();
-error STAKE_IS_LESS_THAN_ZERO();
-error AMOUNT_IS_GRATER_THAN_STAKED();
+import "../errors/ErrorHandlers.sol";
 
 contract StakeThisGuy is ERC20 {
     mapping(address => uint256) public staked;
@@ -19,11 +16,11 @@ contract StakeThisGuy is ERC20 {
 
     function stake(uint256 _amount) external {
         if (_amount < 0) {
-            revert AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
+            revert ErrorHandlers.AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
         }
 
         if (balanceOf(msg.sender) < _amount) {
-            revert BALANCE_IS_LESS_THAN_AMOUNT();
+            revert ErrorHandlers.BALANCE_IS_LESS_THAN_AMOUNT();
         }
 
         _transfer(msg.sender, address(this), _amount);
@@ -39,11 +36,11 @@ contract StakeThisGuy is ERC20 {
 
     function unstake(uint256 _amount) external {
         if (_amount < 0) {
-            revert AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
+            revert ErrorHandlers.AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
         }
 
         if (staked[msg.sender] < _amount) {
-            revert AMOUNT_IS_GRATER_THAN_STAKED();
+            revert ErrorHandlers.AMOUNT_IS_GRATER_THAN_STAKED();
         }
 
         claim();
@@ -55,12 +52,19 @@ contract StakeThisGuy is ERC20 {
 
     function claim() public {
         if (staked[msg.sender] <= 0) {
-            revert STAKE_IS_LESS_THAN_ZERO();
+            revert ErrorHandlers.STAKE_IS_LESS_THAN_ZERO();
         }
 
+        // This represents the duration for which the tokens have been staked.
         uint256 duration = block.timestamp - stakedFromTimeStamp[msg.sender];
+        // It is calculated by subtracting the timestamp when the tokens were staked (stakedFromTimeStamp[msg.sender]) from the current block timestamp.
+        // The result is a duration measured in seconds.
 
+        // So, when you multiply the amount staked (staked[msg.sender]) by the duration in seconds (duration), you get the total "staking time" in seconds for which the tokens have been staked.
+        // Then, dividing this by 3.154e7 effectively converts the staking time from seconds to years.
         uint256 rewards = (staked[msg.sender] * duration) / 3.154e7;
+        // 3.154e7: This is a scientific notation representing the number of seconds in a year.
+        // Specifically, 3.154e7 equals 31,540,000 seconds, which is the approximate number of seconds in a standard Gregorian calendar year (365 days).
 
         _mint(msg.sender, rewards);
 
---
+++ errors/ErrorHandlers.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ErrorHandlers {
+    error AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
+    error BALANCE_IS_LESS_THAN_AMOUNT();
+    error STAKE_IS_LESS_THAN_ZERO();
+    error AMOUNT_IS_GRATER_THAN_STAKED();
+}
