9bd109e80472497ddfad4aa0a13787aa747fc055
Abdullahi Salihu <abdullahisalihuinusa@gmail.com>
2024-02-17 18:18:53 +0100

    Spacing

--- contracts/StakeThisGuy.sol
+++ contracts/StakeThisGuy.sol
@@ -4,9 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 error AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
+error BALANCE_IS_LESS_THAN_AMOUNT();
+error STAKE_IS_LESS_THAN_ZERO();
+error AMOUNT_IS_GRATER_THAN_STAKED();
 
 contract StakeThisGuy is ERC20 {
     mapping(address => uint256) public staked;
+
     mapping(address => uint256) public stakedFromTimeStamp;
 
     constructor(uint256 _amountToMint) ERC20("StakeThisGuy", "STG") {
@@ -17,7 +21,11 @@ contract StakeThisGuy is ERC20 {
         if (_amount < 0) {
             revert AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
         }
-        require(balanceOf(msg.sender) >= _amount, "balance is <= amount");
+
+        if (balanceOf(msg.sender) < _amount) {
+            revert BALANCE_IS_LESS_THAN_AMOUNT();
+        }
+
         _transfer(msg.sender, address(this), _amount);
 
         if (staked[msg.sender] > 0) {
@@ -25,6 +33,7 @@ contract StakeThisGuy is ERC20 {
         }
 
         stakedFromTimeStamp[msg.sender] = block.timestamp;
+
         staked[msg.sender] += _amount;
     }
 
@@ -32,17 +41,29 @@ contract StakeThisGuy is ERC20 {
         if (_amount < 0) {
             revert AMOUNT_IS_LESS_THAN_OR_EQUAL_TO_ZERO();
         }
-        require(staked[msg.sender] >= _amount, "amount is > staked");
+
+        if (staked[msg.sender] < _amount) {
+            revert AMOUNT_IS_GRATER_THAN_STAKED();
+        }
+
         claim();
+
         staked[msg.sender] -= _amount;
+
         _transfer(address(this), msg.sender, _amount);
     }
 
     function claim() public {
-        require(staked[msg.sender] > 0, "staked is <= 0");
+        if (staked[msg.sender] <= 0) {
+            revert STAKE_IS_LESS_THAN_ZERO();
+        }
+
         uint256 duration = block.timestamp - stakedFromTimeStamp[msg.sender];
+
         uint256 rewards = (staked[msg.sender] * duration) / 3.154e7;
+
         _mint(msg.sender, rewards);
+
         stakedFromTimeStamp[msg.sender] = block.timestamp;
     }
 }
